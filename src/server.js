33dce992280609a10115fe589b4d350df92ed903
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-02-25 17:40:06 -0300

    create json db

--- src/server.js
+++ src/server.js
@@ -1,30 +1,36 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js';
+import { Database } from './database.js';
 
-const users = [];
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
-  const { method, url } = req;
+  const { method, url } = req
 
   await json(req, res)
 
   if(method == 'GET' && url == '/users') {
-    return res.end(JSON.stringify(users));
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
   }
 
   if(method == 'POST' && url == '/users') {
     const { name, email } = req.body
 
-    users.push({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email
-    });
+    }
 
-    return res.writeHead(201).end();
+    database.insert('users', user)
+
+    return res.writeHead(201).end()
   }
 
-  return res.writeHead(404).end('Not found');
+  return res.writeHead(404).end('Not found')
 })
 
 server.listen(3333);
